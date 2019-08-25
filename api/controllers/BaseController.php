a717663f1cdfb542ddc4c6d8c0eb2b5748fe1176
nomadurl <ilja.uralsk@mail.ru>
2019-08-25 18:18:52 +0300

    begin api controller

--- api/controllers/BaseController.php
+++ api/controllers/BaseController.php
@@ -1,16 +1,12 @@
 <?php
-namespace backend\controllers;
 
-use Yii;
-use yii\web\Controller;
+namespace api\controllers;
+
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
-use common\models\LoginForm;
+use yii\rest\ActiveController;
 
-/**
- * Site controller
- */
-class SiteController extends Controller
+class BaseController extends ActiveController
 {
     /**
      * {@inheritdoc}
@@ -51,50 +47,5 @@ class SiteController extends Controller
                 'class' => 'yii\web\ErrorAction',
             ],
         ];
-    }
-
-    /**
-     * Displays homepage.
-     *
-     * @return string
-     */
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
-
-    /**
-     * Login action.
-     *
-     * @return string
-     */
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
-
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        } else {
-            $model->password = '';
-
-            return $this->render('login', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Logout action.
-     *
-     * @return string
-     */
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
-
-        return $this->goHome();
     }
 }
